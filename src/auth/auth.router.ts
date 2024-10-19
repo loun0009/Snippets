bdd66391eea2071ae66766b0d40069a245e62e97
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-10-19 19:29:06 +0200

    feat: route post pour le login

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import expressAsyncHandler from "express-async-handler";
 import { authController } from './auth.controller';
 
 const authRouter = express.Router();
+
 authRouter.get('/login',
     expressAsyncHandler(authController.loginForm)
 );
+
+authRouter.post('/login',
+    urlencoded({ extended: true }),
+    expressAsyncHandler(authController.login)
+)
 
 export default authRouter;
